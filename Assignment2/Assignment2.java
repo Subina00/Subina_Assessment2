6d9db82639608762f55ebe26495c5667a39758b6
Subina00 <140357586+Subina00@users.noreply.github.com>
2023-09-28 20:23:47 +1000

    .

--- Assignment2/Assignment2.java
+++ Assignment2/Assignment2.java
@@ -6,58 +6,27 @@ import java.io.*;
  * @author (Subina)
  * @version (a version number or a)
  */
-public class Assignment2
-{
-    // instance variables - replace the example below with your own
 
-    /**
-     * Constructor for objects of class Assignment2.
-     */
-    public Assignment2()
-    {
-        // initialise instance variables
+public class Assignment2 {
 
-    }
+    // Defined data structure to store student information
+    static class Student {
+        String lastName, firstName, studentID, A1, A2, A3;
+        double[] marks;
 
-    public void readFromFile() throws Exception {
-        List<Student> students = new ArrayList<>();
-        Scanner fileScanner = new Scanner(new File("Studentsfile.csv"));
-
-        while (fileScanner.hasNextLine()) {
-
-            String line = fileScanner.nextLine();
-
-            if (line.startsWith("#")) { // Ignore comment lines
-                continue;
-            }
-
-            String[] values = line.split(",");
-            if (values.length != 6) { // Ensuring valid data ignores the empty cells replace this by changing the null values to 0
-                continue;
-            }
-
-            String lastName = values[0].trim();
-            String firstName = values[1].trim();
-            String studentID = values[2].trim();
-            try{
-                double mark1 = Double.parseDouble(values[3].trim());
-                Student student = new Student(firstName,lastName, studentID, mark1);
-                students.add(student);
-            }catch(NumberFormatException e){
-                System.out.println( "");
-            }
+        public Student(String lastName, String firstName, String studentID, double[] marks) {
+            this.lastName = lastName;
+            this.firstName = firstName;
+            this.studentID = studentID;
+            this.marks = marks;
         }
-
-        fileScanner.close();
-        printStudents(students);
-    }
-
-    public  void printStudents(List<Student> students) {
-        for (Student s : students) {
-            System.out.println( s.firstName+" "+" "+s.studentID+ " "+s.assign1);
+        // Function to get total marks
+        public double getTotalMarks() {
+            double total = 0;
+            for (double mark : marks) {
+                total += mark;
+            }
+            return total;
         }
     }
-
-
-
 }
