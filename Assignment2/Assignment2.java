86516348810c604a1bad12fa502bdc0bc9bf9dd7
Subina00 <140357586+Subina00@users.noreply.github.com>
2023-09-28 22:35:20 +1000

    read file

--- Assignment2/Assignment2.java
+++ Assignment2/Assignment2.java
@@ -29,4 +29,44 @@ public class Assignment2 {
             return total;
         }
     }
+    
+     // Function to read data from file
+static List<Student> readDataFromFile(String fileName) throws IOException {
+    List<Student> students = new ArrayList<>();
+    BufferedReader reader = new BufferedReader(new FileReader(fileName));
+    String line;
+
+    while ((line = reader.readLine()) != null) {
+        if (!line.startsWith("//")) {
+            String[] parts = line.split(",");
+            if (parts.length >= 4) {
+                String lastName = parts[0];
+                String firstName = parts[1];
+                String studentID = parts[2];
+                double[] marks = new double[parts.length - 3];
+
+                for (int i = 3; i < parts.length; i++) {
+                    try {
+                        if (parts[i].isEmpty()) {
+                            // Handle empty cells by considering them as 0
+                            marks[i - 3] = 0.0;
+                        } else {
+                            marks[i - 3] = Double.parseDouble(parts[i]);
+                        }
+                    } catch (NumberFormatException e) {
+                        System.err.println("Error parsing marks for student: " + line);
+                        break;
+                    }
+                }
+
+                students.add(new Student(lastName, firstName, studentID, marks));
+            } else {
+                System.err.println("Skipped line with insufficient data: " + line);
+            }
+        }
+    }
+
+    reader.close();
+    return students;
+}
 }
